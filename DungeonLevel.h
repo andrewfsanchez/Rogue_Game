45e2b3a764c189eba841770be73c6011e556589b
winansb <32299607+winansb@users.noreply.github.com>
2018-04-20 16:54:32 -0400

    This is the start of the level design
    Rough idea of what will be needed to build the level

---
+++ DungeonLevel.h
@@ -0,0 +1,53 @@
+//
+// Created by Ben on 4/20/2018.
+//
+
+#ifndef UNTITLED_DUNGEONLEVEL_H
+#define UNTITLED_DUNGEONLEVEL_H
+#include <random>
+#include <vector>
+#include <iostream>
+
+struct room
+{
+    int x,y;
+    int width,height;
+};
+
+class Dungeon
+{
+public:
+    enum Tile
+    {
+        Unused = '',
+        Floor = '.',
+        Wall = '#',
+        ClosedDoor = '+',
+        OpenDoor = '=',
+    };
+
+    enum Direction
+    {
+        North,
+        South,
+        East,
+        West,
+        DirectionCount
+    };
+public:
+    void generate (int maxFeatures);
+    void print();
+    bool createFeature;
+    bool makeRoom;
+    bool placeRoom;
+    bool placeObject;
+
+private:
+    int _width, _height;
+    std::vector<Node> _tiles;
+    std::vector<room> _rooms;
+    std::vector<room> _exits;
+
+};
+
+#endif //UNTITLED_DUNGEONLEVEL_H
