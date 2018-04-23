794a70d00e15b3072b2e8c1ff98ff852c2d6f780
BenV <BenjaminVelliquette@gmail.com>
2018-04-22 23:19:58 -0400

    Empty enemy vector function

--- Game.cpp
+++ Game.cpp
@@ -59,6 +59,11 @@ void Game::moveEnemies()
 	
 }
 
+void Game::eraseEnemies()
+{
+	for (int i = enemies.size() - 1; i >= 0; i--) enemies.erase(enemies.begin() + i);
+}
+
 void Game::playerAction() 
 {
 	bool check = true;
@@ -120,6 +125,7 @@ void Game::updateGrid()
 	/*if(player->getX()==exitX || player->getY()==exitY)
 	{
 		makeNextFloor(floor, diff, player);
+		eraseEnemies();
 		playerAction();
 		return;
 	}
